fcfd4fc662732507ec0bb90f5910026b1d09e0d9
remorses <beats.by.morse@gmail.com>
2025-08-26 18:13:59 +0200

    more padding

--- termcast/src/internal/providers.tsx
+++ termcast/src/internal/providers.tsx
@@ -1,14 +1,14 @@
-import React, { type ReactNode } from "react"
-import { DialogProvider } from "@termcast/api/src/internal/dialog"
+import React, { type ReactNode } from 'react'
+import { DialogProvider } from '@termcast/api/src/internal/dialog'
 
 interface ProvidersProps {
-  children: ReactNode
+    children: ReactNode
 }
 
 export function Providers(props: ProvidersProps): any {
-  return (
-    <DialogProvider>
-      {props.children}
-    </DialogProvider>
-  )
+    return (
+        <DialogProvider>
+            <group padding={2}>{props.children}</group>
+        </DialogProvider>
+    )
 }
