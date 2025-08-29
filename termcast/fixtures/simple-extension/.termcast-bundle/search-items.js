aeb750b97f2b7fd260ae82719122a6249bfc1f2f
remorses <beats.by.morse@gmail.com>
2025-08-29 13:19:35 +0200

    fix termcast resolve issues

--- termcast/fixtures/simple-extension/.termcast-bundle/search-items.js
+++ termcast/fixtures/simple-extension/.termcast-bundle/search-items.js
@@ -1,5 +1,53 @@
-// @bun
-// globals:@termcast/api
+// @bun @bun-cjs
+(function(exports, require, module, __filename, __dirname) {var __create = Object.create;
+var __getProtoOf = Object.getPrototypeOf;
+var __defProp = Object.defineProperty;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __toESM = (mod, isNodeMode, target) => {
+  target = mod != null ? __create(__getProtoOf(mod)) : {};
+  const to = isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target;
+  for (let key of __getOwnPropNames(mod))
+    if (!__hasOwnProp.call(to, key))
+      __defProp(to, key, {
+        get: () => mod[key],
+        enumerable: true
+      });
+  return to;
+};
+var __moduleCache = /* @__PURE__ */ new WeakMap;
+var __toCommonJS = (from) => {
+  var entry = __moduleCache.get(from), desc;
+  if (entry)
+    return entry;
+  entry = __defProp({}, "__esModule", { value: true });
+  if (from && typeof from === "object" || typeof from === "function")
+    __getOwnPropNames(from).map((key) => !__hasOwnProp.call(entry, key) && __defProp(entry, key, {
+      get: () => from[key],
+      enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+    }));
+  __moduleCache.set(from, entry);
+  return entry;
+};
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, {
+      get: all[name],
+      enumerable: true,
+      configurable: true,
+      set: (newValue) => all[name] = () => newValue
+    });
+};
+
+// fixtures/simple-extension/src/search-items.tsx
+var exports_search_items = {};
+__export(exports_search_items, {
+  default: () => SearchItems
+});
+module.exports = __toCommonJS(exports_search_items);
+
+// globals:@termcast/cli
 var AI = globalThis.termcastApi.AI;
 var Action = globalThis.termcastApi.Action;
 var ActionPanel = globalThis.termcastApi.ActionPanel;
@@ -37,17 +85,20 @@ var clearClipboard = globalThis.termcastApi.clearClipboard;
 var confirmAlert = globalThis.termcastApi.confirmAlert;
 var copyTextToClipboard = globalThis.termcastApi.copyTextToClipboard;
 var copyToClipboard = globalThis.termcastApi.copyToClipboard;
-var api_default = globalThis.termcastApi.default;
+var cli_default = globalThis.termcastApi.default;
 var environment = globalThis.termcastApi.environment;
 var getApplications = globalThis.termcastApi.getApplications;
 var getDefaultApplication = globalThis.termcastApi.getDefaultApplication;
 var getFrontmostApplication = globalThis.termcastApi.getFrontmostApplication;
 var getIconEmoji = globalThis.termcastApi.getIconEmoji;
+var getPreferenceValues = globalThis.termcastApi.getPreferenceValues;
 var getSelectedFinderItems = globalThis.termcastApi.getSelectedFinderItems;
 var getSelectedText = globalThis.termcastApi.getSelectedText;
 var logger = globalThis.termcastApi.logger;
 var moveToTrash = globalThis.termcastApi.moveToTrash;
 var open = globalThis.termcastApi.open;
+var openCommandPreferences = globalThis.termcastApi.openCommandPreferences;
+var openExtensionPreferences = globalThis.termcastApi.openExtensionPreferences;
 var openFile = globalThis.termcastApi.openFile;
 var openInBrowser = globalThis.termcastApi.openInBrowser;
 var pasteContent = globalThis.termcastApi.pasteContent;
@@ -164,6 +215,4 @@ function SearchItems() {
     ]
   }, undefined, true, undefined, this);
 }
-export {
-  SearchItems as default
-};
+})
