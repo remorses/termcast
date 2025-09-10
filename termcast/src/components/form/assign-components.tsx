c0433ede27168886ce1516f4c6c731ceac0068f8
remorses <beats.by.morse@gmail.com>
2025-09-10 16:58:46 +0200

    fix tsc

--- termcast/src/components/form/assign-components.tsx
+++ termcast/src/components/form/assign-components.tsx
@@ -8,8 +8,9 @@ import { DatePicker } from './date-picker'
 import { TagPicker } from './tagpicker'
 import { Separator } from './separator'
 import { Description } from './description'
+import { FilePicker } from './file-picker'
 
-// Assign all form components to Form object
+
 Form.TextField = TextField as any
 Form.PasswordField = PasswordField as any
 Form.TextArea = TextArea as any
@@ -17,6 +18,6 @@ Form.Checkbox = Checkbox as any
 Form.Dropdown = Dropdown
 Form.DatePicker = DatePicker
 Form.TagPicker = TagPicker
-Form.FilePicker = null // TODO: implement
+Form.FilePicker = FilePicker
 Form.Separator = Separator
 Form.Description = Description
