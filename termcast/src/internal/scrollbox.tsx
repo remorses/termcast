f2f5ae87ed08d67bf45ca19ce3c352eb7f1419ff
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-10-03 17:18:27 +0200

    Update scrollbox.tsx

--- termcast/src/internal/scrollbox.tsx
+++ termcast/src/internal/scrollbox.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
 
 interface ScrollBoxProps {
   children?: React.ReactNode
@@ -8,44 +8,46 @@ interface ScrollBoxProps {
   style?: any
 }
 
-export const ScrollBox = forwardRef<any, ScrollBoxProps>(
-  ({ children, focused = false, flexGrow, flexShrink, style, ...props }, ref) => {
-    return (
-      <scrollbox
-        ref={ref}
-        focused={focused}
-        flexGrow={flexGrow}
-        flexShrink={flexShrink}
-        style={{
-          rootOptions: {
-            backgroundColor: '#1a1b26',
-            ...(style?.rootOptions || {}),
+export function ScrollBox({
+  children,
+  focused = false,
+  flexGrow,
+  flexShrink,
+  style,
+  ...props
+}: ScrollBoxProps): any {
+  return (
+    <scrollbox
+      focused={focused}
+      flexGrow={flexGrow}
+      flexShrink={flexShrink}
+      style={{
+        rootOptions: {
+          backgroundColor: '#1a1b26',
+          ...(style?.rootOptions || {}),
+        },
+        viewportOptions: {
+          flexGrow: 1,
+          flexShrink: 1,
+          ...(style?.viewportOptions || {}),
+        },
+        contentOptions: {
+          flexShrink: 0,
+          ...(style?.contentOptions || {}),
+        },
+        scrollbarOptions: {
+          visible: true,
+          showArrows: true,
+          trackOptions: {
+            foregroundColor: '#7aa2f7',
+            backgroundColor: '#414868',
           },
-          viewportOptions: {
-            flexGrow: 1,
-            flexShrink: 1,
-            ...(style?.viewportOptions || {}),
-          },
-          contentOptions: {
-            flexShrink: 0,
-            ...(style?.contentOptions || {}),
-          },
-          scrollbarOptions: {
-            visible: true,
-            showArrows: true,
-            trackOptions: {
-              foregroundColor: '#7aa2f7',
-              backgroundColor: '#414868',
-            },
-            ...(style?.scrollbarOptions || {}),
-          },
-        }}
-        {...props}
-      >
-        {children}
-      </scrollbox>
-    )
-  },
-)
-
-ScrollBox.displayName = 'ScrollBox'
+          ...(style?.scrollbarOptions || {}),
+        },
+      }}
+      {...props}
+    >
+      {children}
+    </scrollbox>
+  )
+}
