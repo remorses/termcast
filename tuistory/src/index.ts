44604a97032b59cbb9012e787e186bf99734b0ba
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-12-04 14:30:59 +0100

    remove tuistory

--- tuistory/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { Session, type LaunchOptions, type TextOptions } from './session.js'
-
-import { Session, type LaunchOptions } from './session.js'
-
-export async function launchTerminal(options: LaunchOptions): Promise<Session> {
-  const session = new Session(options)
-  await session.waitIdle()
-  return session
-}
