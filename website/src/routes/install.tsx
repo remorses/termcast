d0dd7a83f969c217a66b66dd586ea32fb6422ed6
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-12-15 14:25:10 +0100

    install script

---
+++ website/src/routes/install.tsx
@@ -0,0 +1,12 @@
+import type { LoaderFunctionArgs } from 'react-router'
+import { generateInstallScript } from '../lib/generate-install-script'
+
+export async function loader({}: LoaderFunctionArgs) {
+  const script = generateInstallScript('remorses/termcast')
+
+  return new Response(script, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  })
+}
